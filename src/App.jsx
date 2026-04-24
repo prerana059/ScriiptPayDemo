2907999555216db7e152cfb41605a4696938ee35
Prerana <prebhme@gmail.com>
2026-04-24 18:50:42 +0545

    created landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 
 import logo from './assets/logo.png';
+import landingpage1 from "./assets/landingpage1.png";
 import { useNavigate } from "react-router-dom";
 
 function App() {
    const navigate = useNavigate();
    
   return(
-    <div>
+    <div className='mx-20'>
       <header className="flex items-center justify-between px-6 py-4 shadow-md">
   
   {/* Left side: Logo */}
   <div className="flex items-center gap-2">
-    <img src={logo} alt="company logo" className="h-20 w-auto object-contain" />
+    <img src={logo} alt="company logo" className="h-15 w-auto object-contain" />
   </div>
 
   {/* Middle: Navigation */}
@@ -54,6 +55,31 @@ function App() {
   </div>
 
 </header>
+
+    <div className="flex w-full">
+  
+  {/* Left content */}
+  <div className="w-1/2">
+    <h1 className="text-5xl font-bold text-blue-600 mt-30">Advanced Payment Solutions</h1>
+    <h1 className="text-5xl font-bold ">for India's boldest Disrupters</h1>
+    <p>100+ Payment Methods | Easy Integration | Powerful Dashboard</p>
+    <button class="inline-flex items-center px-4 py-3 bg-blue-600 text-white rounded hover:bg-blue-700 transition mt-5">
+      <span>Sign Up Now  </span>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640" className='ml-2 size-5'><path d="M566.6 342.6C579.1 330.1 579.1 309.8 566.6 297.3L406.6 137.3C394.1 124.8 373.8 124.8 361.3 137.3C348.8 149.8 348.8 170.1 361.3 182.6L466.7 288L96 288C78.3 288 64 302.3 64 320C64 337.7 78.3 352 96 352L466.7 352L361.3 457.4C348.8 469.9 348.8 490.2 361.3 502.7C373.8 515.2 394.1 515.2 406.6 502.7L566.6 342.7z"/></svg>
+      </button>
+  </div>
+
+  {/* Right image */}
+  <div className="w-1/2 flex justify-end">
+    <img 
+      src={landingpage1} 
+      alt="landing" 
+      className="h-[600px] w-auto object-contain"
+    />
+  </div>
+
+</div>
+
     </div>
   );
 
